087b6aa40a1bd97b92bd796a54b168a3d7eea8d3
Sugan Gopalakrishnan <rgk.sugan@gmail.com>
2014-07-19 13:08:01 +0530

    fix jshint errors in value

--- value/index.js
+++ value/index.js
@@ -1,57 +1,55 @@
 'use strict';
 var util = require('util'),
-  ScriptBase = require('../script-base.js'),
-  path = require('path'),
-  angularUtils = require('../util.js');
+    ScriptBase = require('../script-base.js'),
+    path = require('path'),
+    angularUtils = require('../util.js');
 
 var Generator = module.exports = function Generator() {
-  ScriptBase.apply(this, arguments);
+    ScriptBase.apply(this, arguments);
 
-  this.sourceRoot(path.join(__dirname, '../templates'));
+    this.sourceRoot(path.join(__dirname, '../templates'));
 
-  if (typeof this.env.options.appPath === 'undefined') {
-    try {
-      this.env.options.appPath = require(path.join(process.cwd(), 'bower.json')).appPath;
-    } catch (e) {}
-    this.env.options.appPath = this.env.options.appPath || 'app';
-  }
+    if (typeof this.env.options.appPath === 'undefined') {
+        try {
+            this.env.options.appPath = require(path.join(process.cwd(), 'bower.json')).appPath;
+        } catch (e) {
+
+        }
+        this.env.options.appPath = this.env.options.appPath || 'app';
+    }
 };
 
 util.inherits(Generator, ScriptBase);
 
 Generator.prototype.askForConstantValue = function askFor() {
-  var cb = this.async();
+    var cb = this.async();
 
-  this.prompt([{  
-    type: "input",
-    name: "value",
-    message: "Enter the value for the value '" + this.name + "' : "  
-  }], function (props) {
-    
-    this.constant_value = props.value;
-
-    if (typeof(this.constant_value) === 'string') {
-    	this.constant_value = "'" + this.constant_value + "'";    	
-    }
-
-    cb();
-  }.bind(this));
+    this.prompt([{  
+        type: 'input',
+        name: 'value',
+        message: 'Enter the value for the value \'' + this.name + '\' : '
+    }], function (props) {
+        this.constant_value = props.value;
+        if (typeof (this.constant_value) === 'string') {
+            this.constant_value = '\'' + this.constant_value + '\'';
+        }
+        cb();
+    }.bind(this));
 };
 
-
 Generator.prototype.createConstant = function createViewFiles() {
-  
-  var config = {
-    file: path.join(
-      this.env.options.appPath,
-      'js/main.js'
-    ),
-    needle: '/* ---> Do not delete this comment (Values) <--- */',
-    splicable: [
-      "app.value('"+this.name+"', "+this.constant_value+");"
-    ]
-  };
 
-  angularUtils.rewriteFile(config);
-  console.log('Added Value :  '+ this.name);
+    var config = {
+        file: path.join(
+            this.env.options.appPath,
+            'js/main.js'
+        ),
+        needle: '/* ---> Do not delete this comment (Values) <--- */',
+        splicable: [
+            'app.value(\'' + this.name + '\', ' + this.constant_value + ');'
+        ]
+    };
+
+    angularUtils.rewriteFile(config);
+    console.log('Added Value :  '+ this.name);
 };
