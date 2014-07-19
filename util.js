d41758ad9468e9a0891f11c72a75b1b9ad3eb5f7
Sugan Gopalakrishnan <rgk.sugan@gmail.com>
2014-07-19 13:39:28 +0530

    js hint fixes few more

--- util.js
+++ util.js
@@ -2,76 +2,75 @@
 var path = require('path');
 var fs = require('fs');
 
-
 module.exports = {
-  rewrite: rewrite,
-  rewriteFile: rewriteFile,
-  appName: appName
+    rewrite: rewrite,
+    rewriteFile: rewriteFile,
+    appName: appName
 };
 
 function rewriteFile (args) {
-  args.path = args.path || process.cwd();
-  var fullPath = path.join(args.path, args.file);
+    args.path = args.path || process.cwd();
+    var fullPath = path.join(args.path, args.file);
 
-  args.haystack = fs.readFileSync(fullPath, 'utf8');
-  var body = rewrite(args);
+    args.haystack = fs.readFileSync(fullPath, 'utf8');
+    var body = rewrite(args);
 
-  fs.writeFileSync(fullPath, body);
+    fs.writeFileSync(fullPath, body);
 }
 
 function escapeRegExp (str) {
-  return str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, '\\$&');
+    return str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, '\\$&');
 }
 
 function rewrite (args) {
-  // check if splicable is already in the body text
-  var re = new RegExp(args.splicable.map(function (line) {
-    return '\s*' + escapeRegExp(line);
-  }).join('\n'));
+    // check if splicable is already in the body text
+    var re = new RegExp(args.splicable.map(function (line) {
+        return '\s*' + escapeRegExp(line);
+    }).join('\n'));
 
-  if (re.test(args.haystack)) {
-    return args.haystack;
-  }
-
-  var lines = args.haystack.split('\n');
-
-  var otherwiseLineIndex = 0;
-  lines.forEach(function (line, i) {
-    if (line.indexOf(args.needle) !== -1) {
-      otherwiseLineIndex = i;
+    if (re.test(args.haystack)) {
+        return args.haystack;
     }
-  });
 
-  var spaces = 0;
-  while (lines[otherwiseLineIndex].charAt(spaces) === ' ') {
-    spaces += 1;
-  }
+    var lines = args.haystack.split('\n');
 
-  var spaceStr = '';
-  while ((spaces -= 1) >= 0) {
-    spaceStr += ' ';
-  }
+    var otherwiseLineIndex = 0;
+    lines.forEach(function (line, i) {
+        if (line.indexOf(args.needle) !== -1) {
+            otherwiseLineIndex = i;
+        }
+    });
 
-  lines.splice(otherwiseLineIndex, 0, args.splicable.map(function (line) {
-    return spaceStr + line;
-  }).join('\n'));
+    var spaces = 0;
+    while (lines[otherwiseLineIndex].charAt(spaces) === ' ') {
+        spaces += 1;
+    }
 
-  return lines.join('\n');
+    var spaceStr = '';
+    while ((spaces -= 1) >= 0) {
+        spaceStr += ' ';
+    }
+
+    lines.splice(otherwiseLineIndex, 0, args.splicable.map(function (line) {
+        return spaceStr + line;
+    }).join('\n'));
+
+    return lines.join('\n');
 }
 
 function appName (self) {
-  var counter = 0, suffix = self.options['app-suffix'];
-  
-  // Have to check this because of generator bug #386
-  process.argv.forEach(function(val) {
-    if (val.indexOf('--app-suffix') > -1) {
-      counter++;
-    }
-  });
+    var counter = 0, suffix = self.options['app-suffix'];
 
-  // if (counter === 0 || (typeof suffix === 'boolean' && suffix)) {  
-  if (typeof suffix === 'boolean' && suffix) {
-    suffix = 'App';
-  }
-  return suffix ? self._.classify(suffix) : '';
+    // Have to check this because of generator bug #386
+    process.argv.forEach(function(val) {
+        if (val.indexOf('--app-suffix') > -1) {
+            counter++;
+        }
+    });
+
+    // if (counter === 0 || (typeof suffix === 'boolean' && suffix)) {  
+    if (typeof suffix === 'boolean' && suffix) {
+        suffix = 'App';
+    }
+    return suffix ? self._.classify(suffix) : '';
 }
--- view/index.js
+++ view/index.js
@@ -1,51 +1,53 @@
 'use strict';
 var path = require('path'),
-  util = require('util'),
-  yeoman = require('yeoman-generator'),
-  ScriptBase = require('../script-base.js');
+    util = require('util'),
+    yeoman = require('yeoman-generator'),
+    ScriptBase = require('../script-base.js');
 
 var Generator = module.exports = function Generator() {
-  ScriptBase.apply(this, arguments);
-  
-  this.option('c', {
-    desc: 'Skip adding the controller',
-    type: String,
-    required: 'true'
-  });
+    ScriptBase.apply(this, arguments);
 
-  this.option('css', {
-    desc: 'Skip adding css file',
-    type: String,
-    required: 'true'
-  });
+    this.option('c', {
+        desc: 'Skip adding the controller',
+        type: String,
+        required: 'true'
+    });
 
-  if(!this.options['css']) {
-    this.hookFor('boom:style');  
-  }
+    this.option('css', {
+        desc: 'Skip adding css file',
+        type: String,
+        required: 'true'
+    });
 
-  if(!this.options['c']) {
-    this.hookFor('boom:controller');  
-  }
+    if (!this.options['css']) {
+        this.hookFor('boom:style');  
+    }
 
-  this.sourceRoot(path.join(__dirname, '../templates'));
+    if (!this.options['c']) {
+        this.hookFor('boom:controller');  
+    }
 
-  if (typeof this.env.options.appPath === 'undefined') {
-    try {
-      this.env.options.appPath = require(path.join(process.cwd(), 'bower.json')).appPath;
-    } catch (e) {}
-    this.env.options.appPath = this.env.options.appPath || 'app';
-  }
+    this.sourceRoot(path.join(__dirname, '../templates'));
+
+    if (typeof this.env.options.appPath === 'undefined') {
+        try {
+        this.env.options.appPath = require(path.join(process.cwd(), 'bower.json')).appPath;
+        } catch (e) {
+
+        }
+        this.env.options.appPath = this.env.options.appPath || 'app';
+    }
 };
 
 util.inherits(Generator, ScriptBase);
 
 Generator.prototype.createViewFiles = function createViewFiles() {
-  this.template(
-    'view.html',
-    path.join(
-      this.env.options.appPath,
-      'templates',
-      this.name.toLowerCase() + '.html'
-    )
-  );
+    this.template(
+        'view.html',
+        path.join(
+            this.env.options.appPath,
+            'templates',
+            this.name.toLowerCase() + '.html'
+        )
+    );
 };
